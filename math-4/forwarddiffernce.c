65dece73cce6ed51a7e89100e28da0362fdec73b
jenisha45 <jenishakharbuja0@gmail.com>
2026-08-02 12:34:48 +0545

    Update forwarddiffernce.c
    Refactor code for clarity and correct input prompts.

--- math-4/forwarddiffernce.c
+++ math-4/forwarddiffernce.c
@@ -1,34 +1,43 @@
 #include<stdio.h>
+
 int main()
 {
-  float a[100],b[100],sum=b[0],prod=1,x;
-  int i,j,n;
-  
-  printf("enter the size of array");
-  scanf("%d",&n);
-  
-  printf("enter the first array element");
-  for(i=0;i<n;i++)
-   scanf("%d",&a[i]);
- 
-  printf("enter the second array element");
-  for(i=0;i<n;i++)
-   scanf("%d",&b[i]);
+    float a[100], b[100], sum, prod = 1, x;
+    int i, j, n;
+    
+    printf("enter the size of array: ");
+    scanf("%d",&n);
 
-  printf("enter value of a to find y:");
-  scanf("%d",&x);
-float h=a[1]-a[0];
-  float s=(x-a[0])/h;
-  for(i=0;i<n;i++)
+    printf("enter x values:\n");
+    for(i=0;i<n;i++)
+        scanf("%f",&a[i]);
+
+    printf("enter y values:\n");
+    for(i=0;i<n;i++)
+        scanf("%f",&b[i]);
+
+    printf("enter value of x to find y: ");
+    scanf("%f",&x);
+
+    float h = a[1] - a[0];
+    float s = (x - a[0]) / h;
+
+    sum = b[0];
+
+    // Forward difference table
+    for(i=1;i<n;i++)
     {
-      for(j=0;j<n;j++)
+        for(j=0;j<n-i;j++)
         {
-        b[j]=b[j+1]-b[j];
+            b[j] = b[j+1] - b[j];
         }
-      prod*=s/i;
-      s--;
-      sum+=b[0]*prod;
+
+        prod *= (s/i);
+        s--;
+        sum += b[0] * prod;
     }
-  printf("b(%d)=%f",x,sum);
-  return 0;
-}  
+
+    printf("Interpolated value=%f",sum);
+
+    return 0;
+}
