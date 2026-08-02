74a341c313eaa1e9396afbbd8445ec0866effa75
jenisha45 <jenishakharbuja0@gmail.com>
2026-08-02 11:53:46 +0545

    Allow user input for initial guess and iterations
    Modified main function to accept user input for x0 and maxIter.

--- math-4/netwonraphson.c
+++ math-4/netwonraphson.c
@@ -10,12 +10,14 @@ float dt(float x)
 }   
 int main()
 {
-    float x0=5, x1, err, E=1e-20;
-    int maxIter=20, i= 1;
+    float x0, x1, err, E=1e-20;
+    int maxIter, i= 1;
+    printf("Enter the value of x0 and maximum iteration=");
+    scanf("%f %d",&x0,&maxIter);
     do
     {
      x1=x0-f(x0)/df(x0);
-      printf("Iteration %d x_%d=%f\n",i,i,x1);
+      printf("Iteration %d =%f\n",i,x1);
       err=(x1-x0)/x1;
       printf("Error:%f\n",err);
       x0=x1;
