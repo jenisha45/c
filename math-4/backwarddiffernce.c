9af5474d3ba204a4843ca8cb3c65a1b31093ec15
jenisha45 <jenishakharbuja0@gmail.com>
2026-08-02 13:11:36 +0545

    fix

--- math-4/backwarddiffernce.c
+++ math-4/backwarddiffernce.c
@@ -1,34 +1,41 @@
 #include<stdio.h>
+
 int main()
 {
-  float a[100],b[100],sum=b[0],prod=1,x;
-  int i,j,n;
-  
-  printf("enter the size of array");
-  scanf("%d",&n);
-  
-  printf("enter the first array element");
-  for(i=0;i<n;i++)
-   scanf("%d",&a[i]);
- 
-  printf("enter the second array element");
-  for(i=0;i<n;i++)
-   scanf("%d",&b[i]);
+    float a[100], b[100], sum, prod = 1, x;
+    int i, j, n;
 
-  printf("enter value of a to find y:");
-  scanf("%d",&x);
-float h=a[1]-a[0];
-  float s=(x-a[0])/h;
-  for(i=0;i<n;i++)
+    printf("enter the size of array: ");
+    scanf("%d",&n);
+
+    printf("enter the first array element: ");
+    for(i=0;i<n;i++)
+        scanf("%f",&a[i]);
+
+    printf("enter the second array element: ");
+    for(i=0;i<n;i++)
+        scanf("%f",&b[i]);
+
+    printf("enter value of a to find y: ");
+    scanf("%f",&x);
+
+    sum = b[n-1];
+
+    float h = a[1] - a[0];
+    float s = (x - a[n-1]) / h;
+
+    for(i=1;i<n;i++)
     {
-      for(j=0;j<n;j++)
+        for(j=n-1;j>=i;j--)
         {
-        b[j]=b[j+1]-b[j];
+            b[j] = b[j] - b[j-1];
         }
-      prod*=s/i;
-      s++;
-      sum+=b[n-1-i]*prod;
+
+        prod *= (s+i-1)/i;
+        sum += b[n-1] * prod;
     }
-  printf("b(%d)=%f",x,sum);
-  return 0;
-}  
+
+    printf("Interpolated value = %f",sum);
+
+    return 0;
+}
