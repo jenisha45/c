d6441c685aace7760261ced2319e62406b4ea984
jenisha45 <jenishakharbuja0@gmail.com>
2026-08-02 11:59:50 +0545

    Update netwonraphson.c

--- math-4/netwonraphson.c
+++ math-4/netwonraphson.c
@@ -1,30 +1,44 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+
 float f(float x)
 {
-    return pow(x,3) -30 ; // Example function: f(x) = x^3 - 30
-}   
-float dt(float x)
+    return x*x*x - 30;
+}
+
+float df(float x)
 {
-    return 3*pow(x,2); // Example function: f(x) = 3x^2
-}   
+    return 3*x*x;
+}
+
 int main()
 {
-    float x0, x1, err, E=1e-20;
-    int maxIter, i= 1;
-    printf("Enter the value of x0 and maximum iteration=");
-    scanf("%f %d",&x0,&maxIter);
+    float x0, x1, err, E = 1e-5;
+    int maxIter, i = 1;
+
+    printf("Enter initial guess and maximum iterations: ");
+    scanf("%f %d", &x0, &maxIter);
+
     do
     {
-     x1=x0-f(x0)/df(x0);
-      printf("Iteration %d =%f\n",i,x1);
-      err=(x1-x0)/x1;
-      printf("Error:%f\n",err);
-      x0=x1;
-      i++;
-}while(fabs(err)>E && i<maxIter);
+        if (df(x0) == 0)
+        {
+            printf("Derivative is zero. Method fails.\n");
+            return 0;
+        }
 
-printf("The approximate required root:%f ",x1);
- 
-return 0;
+        x1 = x0 - f(x0)/df(x0);
+
+        err = fabs((x1 - x0)/x1);
+
+        printf("Iteration %d : x = %.6f   Error = %.6f\n", i, x1, err);
+
+        x0 = x1;
+        i++;
+
+    } while (err > E && i <= maxIter);
+
+    printf("\nApproximate root = %.6f\n", x1);
+
+    return 0;
 }
