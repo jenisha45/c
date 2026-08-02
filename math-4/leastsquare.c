504478a3e9678b14c3acca996ec733c27358d75b
jenisha45 <jenishakharbuja0@gmail.com>
2026-08-02 13:02:08 +0545

    Fix

--- math-4/leastsquare.c
+++ math-4/leastsquare.c
@@ -4,14 +4,14 @@ int main()
   float x[100],y[100],sumx=0,sumy=0,sumxy=0,sumx2=0,a,b,prod=1;
   int i,n,xx;
   
-  printf("enter the size of array");
+  printf("enter the size of array:");
   scanf("%d",&n);
   
-  printf("enter the first array element");
+  printf("enter the first array element:");
   for(i=0;i<n;i++)
    scanf("%f",&x[i]);
  
-  printf("enter the second array element");
+  printf("enter the second array element:");
   for(i=0;i<n;i++)
    scanf("%f",&y[i]);
 
@@ -24,9 +24,10 @@ int main()
       sumxy+=x[i]*y[i];
       sumx2+=x[i]*x[i];
     }
-  b=(x*sumxy-sumx*sumy)/(x*sumx2-sumx*sumx);
-  a=sumy/n-b*sumx/n;
-  printf("the least square line si:y=%f+%fc.\n",a,b);
-  printf("the predicted value of y=%f\n",a+b*xx);
+  b=(n*sumxy-sumx*sumy)/(n*sumx2-sumx*sumx);
+  a=(sumy/n)-b*(sumx/n);
+  printf("The least square line is: y = %f + %fx\n", a, b);
+
+  printf("The predicted value of y = %f\n", a + b*xx);
   return 0;
 }  
